0cfd01bab6377165812821abea7539586b9e7543
Сергей Тенюшев <leonid777russia@gmail.com>
2023-12-19 13:25:00 +0300

    Добавлен файл 1.py

---
+++ main.cpp
@@ -0,0 +1,15 @@
+def main():
+    a = int(input("Enter A: "))
+    b = int(input("Enter B: "))
+    
+    print(f"A + B = {a + b}")
+    print(f"A * 2 = {a * 2}")
+    print(f"A - B = {a - b}")
+<<<<<<< HEAD
+    print(f"A * B = {a * b}")
+    print(f"A / B = {a / b}")  
+=======
+    print(f"A / B = {a / b}")  
+
+if __name__ == "__main__":
+    main()
